ca06ba6c5870b69759677d9f1757537618b6072b
daniellefernandeszzz77-prog <daniellefernandeszzz77@gmail.com>
2026-08-27 19:01:04 -0300

    Adicionar config.js

---
+++ config.js
@@ -0,0 +1,29 @@
+window.SITE_CONFIG = {
+  brandName: "Conteúdo exclusivo",
+  pixKey: "",
+  instagramUrl: "",
+  receiptApi: "https://area-pacotes.joaogatti170.chatgpt.site/api/receipts",
+  packages: [
+    {
+      id: "pacote-1",
+      name: "Pacote 1",
+      price: "R$ --,--",
+      description: "Nome, valor e descrição serão inseridos com os dados finais.",
+      image: ""
+    },
+    {
+      id: "pacote-2",
+      name: "Pacote 2",
+      price: "R$ --,--",
+      description: "Nome, valor e descrição serão inseridos com os dados finais.",
+      image: ""
+    },
+    {
+      id: "pacote-3",
+      name: "Pacote 3",
+      price: "R$ --,--",
+      description: "Nome, valor e descrição serão inseridos com os dados finais.",
+      image: ""
+    }
+  ]
+};
